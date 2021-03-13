1fb94c23916091aa4f0446511d2cb18fc853310e
walle <walle@evy.null>
2021-03-13 09:53:46 +0800

    初始化 SharpCR.CLI 项目

---
+++ SharpCR.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpCR.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
